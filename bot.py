05005b6c688b829a79245e37118fc428430f45ab
marquisem505 <157083700+marquisem505@users.noreply.github.com>
2025-08-03 19:56:15 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -267,10 +267,11 @@ async def main():
     app.add_handler(CommandHandler("logs", view_logs))
 
     async def telegram_webhook(request):
-        data = await request.json()
-        update = Update.de_json(data, app.bot)
-        await app.update_queue.put(update)
-        return web.Response(text="OK")
+    print("📥 Webhook hit!")
+    data = await request.json()
+    update = Update.de_json(data, app.bot)
+    await app.update_queue.put(update)
+    return web.Response(text="OK")
 
     web_app = web.Application()
     web_app.router.add_get("/status", healthcheck)
