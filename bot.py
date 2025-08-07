6e57856d577f2e77fc58135bc3bb3913b09d235b
marquisem505 <157083700+marquisem505@users.noreply.github.com>
2025-08-07 01:55:20 -0400

    update boy.py

--- bot.py
+++ bot.py
@@ -496,7 +496,7 @@ async def healthcheck(request):
 async def main():
     print("✅ Scam's Club bot is starting...") 
     app = Application.builder().token(BOT_TOKEN).build()
-    await app.bot.set_webhook(f"{WEBHOOK_URL}/telegram-webhook")
+    await app.bot.set_webhook(f"{WEBHOOK_URL})
     logging.info("Bot started successfully.")
 
     me = await app.bot.get_me()
